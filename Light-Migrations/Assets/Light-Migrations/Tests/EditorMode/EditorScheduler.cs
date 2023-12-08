9ccb54b9fb351eb5f0604dd75b2b0568e5ee3d39
vangogih <vangogih@yandex.ru>
2023-12-08 22:27:58 +0300

    Made bdd tests working in PlayMode

---
+++ Light-Migrations/Assets/Light-Migrations/Tests/EditorMode/EditorScheduler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Responsible;
+using Responsible.Utilities;
+using UnityEditor;
+
+namespace Light_Migrations.Tests.EditorMode
+{
+    public sealed class EditorScheduler : ITestScheduler, IDisposable
+    {
+        public int FrameNow { get; private set; }
+        public DateTimeOffset TimeNow => DateTimeOffset.Now;
+        
+        private readonly RetryingPoller _poller = new();
+
+        public EditorScheduler()
+        {
+            EditorApplication.update += OnUpdate;
+        }
+
+        public void Dispose()
+        {
+            EditorApplication.update -= OnUpdate;
+        }
+
+        public IDisposable RegisterPollCallback(Action action) => _poller.RegisterPollCallback(action);
+        private void OnUpdate()
+        {
+            FrameNow++;
+            _poller.Poll();
+        }
+    }
+}
---
+++ Light-Migrations/Assets/Light-Migrations/Tests/EditorMode/ExampleBddTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using Responsible;
+using static Responsible.Bdd.Keywords;
+
+namespace Light_Migrations.Tests.EditorMode
+{
+    public sealed class ExampleBddTest
+    {
+        private readonly InstructionExecutor _executor = new();
+
+        [Test]
+        public async void Example()
+        {
+            
+            await _executor.RunScenario(
+                Scenario("Example scenario"),
+                Given("the setup is correct", Responsibly.DoAndReturn("setup", () => new object())),
+                When("the user does something", Responsibly.Do("action", () => {
+                    var c = 0;
+                    while (c < 1000000000)
+                        c++;
+                })),
+                Then("the state should be updated correctly", Responsibly.Do("assertion", () => { })));
+            
+        }
+    
+        [TearDown]
+        public void TearDown()
+        {
+            _executor.Dispose();
+        }
+    
+        [SetUp]
+        public void SetUp()
+        {
+        }
+    }
+}
