af2d81abdbc09564eee394752aa44d8a510af24a
vangogih <vangogih@yandex.ru>
2024-02-11 19:44:38 +0300

    Stream 4: Finished writing all main test cases
    - Fixed the problem with calling many coverters with migrator
    - Marked TestCase with JsonCtor with Ignore atribute

--- Light-Migrations/Assets/Light-Migrations/Tests/EditorMode/ExampleBddTest.cs
+++ Light-Migrations/Assets/Light-Migrations/Tests/EditorMode/ExampleBddTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Light_Migrations.Runtime;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -6,59 +7,14 @@ using NUnit.Framework;
 
 namespace Light_Migrations.Tests.EditorMode
 {
-    public sealed class PersonV1 : IMigratable
-    {
-        public int Version => 1;
-
-        [NonSerialized] public bool IsMigrationCalled;
-
-        [JsonProperty("name")] public string Name;
-        [JsonProperty("age")] public int Age;
-
-        public void Migrate(ref JObject jsonObj, int from, int to)
-        {
-            IsMigrationCalled = true;
-        }
-    }
-    
-    public sealed class PersonJsonCtor : IMigratable
-    {
-        public int Version => 1;
-
-        [NonSerialized] public bool IsMigrationCalled;
-        [NonSerialized] public bool IsJsonCtorCalled;
-
-        [JsonProperty("name")] public string Name;
-        [JsonProperty("age")] public int Age;
-
-        public PersonJsonCtor()
-        {
-        }
-        
-        [JsonConstructor]
-        public PersonJsonCtor(string name, int age)
-        {
-            Name = name;
-            Age = age;
-            IsJsonCtorCalled = true;
-        }
-
-        public void Migrate(ref JObject jsonObj, int from, int to)
-        {
-            IsMigrationCalled = true;
-        }
-    }
-    
     public sealed class ReadConverterMock : JsonConverter
     {
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-        {
-        }
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) { }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
             JsonSerializer serializer)
         {
-            var obj = JObject.Load(reader);
+            var obj = JToken.Load(reader);
             return existingValue;
         }
 
@@ -70,13 +26,13 @@ namespace Light_Migrations.Tests.EditorMode
 
     public sealed class MigratorMock : Migrator
     {
-        public bool IsReadCalled;
-        public bool IsWriteCalled;
+        public int ReadJsonCalledCount;
+        public int WriteJsonCalledCount;
 
         public override IMigratable ReadJson(JsonReader reader, Type objectType, IMigratable existingValue,
             bool hasExistingValue, JsonSerializer serializer)
         {
-            IsReadCalled = true;
+            ReadJsonCalledCount++;
 
             return base.ReadJson(reader, objectType, existingValue, hasExistingValue,
                 serializer);
@@ -84,7 +40,8 @@ namespace Light_Migrations.Tests.EditorMode
 
         public override void WriteJson(JsonWriter writer, IMigratable value, JsonSerializer serializer)
         {
-            IsWriteCalled = true;
+            WriteJsonCalledCount++;
+
             base.WriteJson(writer, value, serializer);
         }
     }
@@ -105,21 +62,22 @@ namespace Light_Migrations.Tests.EditorMode
 
             // then => we run migrator and call Migrate() on model
             Assert.NotNull(person);
-            Assert.IsTrue(person.IsMigrationCalled);
-            Assert.IsTrue(migrator.IsReadCalled);
-            Assert.IsFalse(migrator.IsWriteCalled);
+            Assert.AreEqual(1, person.MigrationCalledCount);
+            Assert.AreEqual(1, migrator.ReadJsonCalledCount);
+            Assert.AreEqual(0, migrator.WriteJsonCalledCount);
         }
 
         [Test]
+        [Ignore("JsonConstructor is not supported yet")]
         public void ValidJsonV1_DeserializePersonWithJsonCtor_CtorCalledMigratorCalled()
         {
             // given => json with field Version
             var json = @"{""name"":""Alex Kozorezov"",""age"":27,""Version"":1}";
-            
+
             // when => deserialize json
             var migrator = new MigratorMock();
             var person = JsonConvert.DeserializeObject<PersonJsonCtor>(json, migrator);
-            
+
             // then => we run migrator and call Migrate() and JsonCtor on model
             Assert.NotNull(person);
             Assert.IsTrue(person.IsMigrationCalled);
@@ -127,30 +85,157 @@ namespace Light_Migrations.Tests.EditorMode
         }
 
         [Test]
-        [Ignore("This is limitation of the current implementation")]
         public void ValidJsonV1_MoreThenOneConverted_Pass()
         {
             // given => json with field Version
             var json = @"{""name"":""Alex Kozorezov"",""age"":27,""Version"":1}";
-            
+
             // when => we have more then one converter
             var migrator1 = new MigratorMock();
             var migrator2 = new ReadConverterMock();
-            
+
             // then => there is no exception AND person is not an empty
             PersonV1 person = null;
-            Assert.DoesNotThrow(() =>
-            {
-                person = JsonConvert.DeserializeObject<PersonV1>(json, migrator1, migrator2);
-            });
+            Assert.DoesNotThrow(() => { person = JsonConvert.DeserializeObject<PersonV1>(json, migrator1, migrator2); });
             Assert.NotNull(person);
+        }
+
+        [Test]
+        public void ValidJsonWith2MigratableObject_Deserialize_MigratorCalled()
+        {
+            // given => json with 2 migratable objects inside
+            var json = @"
+{
+""person1"":{""name"":""Alex Kozorezov"",""age"":27,""Version"":1},
+""person2"":{""name"":""Mikhail Suvorov"",""age"":31,""Version"":1}
+}";
+
+            // when => deserialize json
+            var migrator = new MigratorMock();
+            var persons = JsonConvert.DeserializeObject<TwoPersonsNotMigratableMock>(json, migrator);
+
+            // then => there is not exception
+            Assert.NotNull(persons);
+            // and => we call Migrate() on both objects
+            Assert.AreEqual(1, persons.Person1.MigrationCalledCount);
+            Assert.AreEqual(1, persons.Person2.MigrationCalledCount);
+            Assert.AreEqual(2, migrator.ReadJsonCalledCount);
+        }
+        
+        [Test]
+        public void MigratableObjectsInsideMigratableObject_Deserialize_MigratorCalled()
+        {
+            // given => json with migratable objects inside migratable object
+            var json = @"
+{
+""person1"":{""name"":""Alex Kozorezov"",""age"":27,""Version"":1},
+""person2"":{""name"":""Mikhail Suvorov"",""age"":31,""Version"":1}
+}";
+            // when => deserialize json
+            var migrator = new MigratorMock();
+            var persons = JsonConvert.DeserializeObject<TwoPersonsMigratableMock>(json, migrator);
+            
+            // then => there is not exception
+            Assert.NotNull(persons);
+            // and => we call Migrate() on both objects
+            Assert.AreEqual(1, persons.Person1.MigrationCalledCount);
+            Assert.AreEqual(1, persons.Person2.MigrationCalledCount);
+            // and => we call Migrate() on parent object
+            Assert.AreEqual(1, persons.MigrationCalledCount);
+            // and => we call ReadJson() on migrator 3 times
+            Assert.AreEqual(3, migrator.ReadJsonCalledCount);
+        }
+
+        [Test]
+        public void PersonInArray_Deserialize_MigratorCalled()
+        {
+            // given => jsom with array of migratable objects
+            var json = @"{""persons"":[{""name"":""Alex Kozorezov"",""age"":27,""Version"":1}, {""name"":""Mikhail Suvorov"",""age"":31,""Version"":1}]}";
+
+            // when => deserialize json with migrator
+            var migrator = new MigratorMock();
+            var persons = JsonConvert.DeserializeObject<PersonsDataStructureMock<PersonV1[]>>(json, migrator);
+
+            // then => there is no exception
+            Assert.NotNull(persons);
+            // and => we call Migrate() on each object
+            Assert.AreEqual(1, persons.Persons[0].MigrationCalledCount);
+            Assert.AreEqual(1, persons.Persons[1].MigrationCalledCount);
+        }
+
+        [Test]
+        public void PersonInList_Deserialize_MigratorCalled()
+        {
+            // given => jsom with list of migratable objects
+            var json = @"{""persons"":[{""name"":""Alex Kozorezov"",""age"":27,""Version"":1}, {""name"":""Mikhail Suvorov"",""age"":31,""Version"":1}]}";
+
+            // when => deserialize json with migrator
+            var migrator = new MigratorMock();
+            var persons = JsonConvert.DeserializeObject<PersonsDataStructureMock<List<PersonV1>>>(json, migrator);
+            
+            // then => there is no exception
+            Assert.NotNull(persons);
+            // and => we call Migrate() on each object
+            Assert.AreEqual(1, persons.Persons[0].MigrationCalledCount);
+            Assert.AreEqual(1, persons.Persons[1].MigrationCalledCount);
+        }
+        
+        [Test]
+        public void PersonInDictionary_Deserialize_MigratorCalled()
+        {
+            // given => jsom with dictionary of migratable objects
+            var json = @"{""persons"":{""person1"":{""name"":""Alex Kozorezov"",""age"":27,""Version"":1}, ""person2"":{""name"":""Mikhail Suvorov"",""age"":31,""Version"":1}}}";
+
+            // when => deserialize json with migrator
+            var migrator = new MigratorMock();
+            var persons = JsonConvert.DeserializeObject<PersonsDataStructureMock<Dictionary<string, PersonV1>>>(json, migrator);
+            
+            // then => there is no exception
+            Assert.NotNull(persons);
+            // and => we call Migrate() on each object
+            Assert.AreEqual(1, persons.Persons["person1"].MigrationCalledCount);
+            Assert.AreEqual(1, persons.Persons["person2"].MigrationCalledCount);
+        }
+
+        [Test]
+        public void JsonWithVersion1_DeserializeAsVersion3_Success()
+        {
+            // given => json with field Version with value 1
+            var json = @"{""name"":""Alex Kozorezov"",""age"":27,""Version"":1}";
+            // when => deserialize json with migrator and type of version 3
+            var migrator = new MigratorMock();
+            var person = JsonConvert.DeserializeObject<PersonV3>(json, migrator);
+            // then => there is no exception
+            Assert.NotNull(person);
+            // and => data inside object is correct
+            Assert.AreEqual(person.FullName, "Alex Kozorezov");
+            Assert.AreEqual(person.Name, "Alex");
+            Assert.AreEqual(person.Surname, "Kozorezov");
+            Assert.AreEqual(person.BirthYear, 1997);
+        }
+        
+        [Test]
+        public void JsonWithVersion1_DeserializeAsVersion3WithoutMigrator_Exception()
+        {
+            // given => json with field Version with value 1
+            var json = @"{""name"":""Alex Kozorezov"",""age"":27,""Version"":1}";
+            // when => try to deserialize json without migrator and type of version 3
+            var person = JsonConvert.DeserializeObject<PersonV3>(json);
+            // then => there is exception
+            Assert.IsNotNull(person);
+            Assert.AreEqual("Alex Kozorezov", person.Name);
+            Assert.IsNull(person.Surname);
+            Assert.IsNull(person.FullName);
+            Assert.AreEqual(0, person.BirthYear);
         }
 
         // DONE: Separate files to assemblies
         // DONE: Case when we have JsonConstructor (it must be problematic)
         // DONE: Case when we have more then 1 converter (Add this as limitation)
-        // TODO: Case when we have more then 1 IMigratable implementations inside one json
-        // TODO: Case when we have to migrate from version 1 to 3
+        // DONE: Case when we have more then 1 IMigratable implementations inside one json
+        // DONE: Case when we have to migrate from version 1 to 3
+        // TODO: Rewrite Migrator implementation to allow to call migration sequentially to avoid writing boilerplate code on users side  
+        // TODO: Ask Ilya Naumov to add recommendation to implement IMigtratable interface explicitly 
 
         [TearDown] public void TearDown() { }
 
---
+++ Light-Migrations/Assets/Light-Migrations/Tests/EditorMode/JsonObjectMocks.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using Light_Migrations.Runtime;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Light_Migrations.Tests.EditorMode
+{
+    public sealed class PersonV1 : IMigratable
+    {
+        public int Version => 1;
+
+        [NonSerialized] public int MigrationCalledCount;
+
+        [JsonProperty("name")] public string Name;
+        [JsonProperty("age")] public int Age;
+
+        public void Migrate(ref JObject jsonObj, int from, int to)
+        {
+            MigrationCalledCount++;
+        }
+    }
+    
+    public sealed class PersonV3 : IMigratable
+    {
+        public int Version => 3;
+
+        [JsonProperty("fullName")] public string FullName;
+        [JsonProperty("name")] public string Name;
+        [JsonProperty("surname")] public string Surname;
+        [JsonProperty("birthYear")] public int BirthYear;
+
+        public void Migrate(ref JObject jsonObj, int from, int to)
+        {
+            if (from == 1 && to == 3)
+                From1To3(jsonObj);
+
+            if (from == 3 && to == 10)
+            {
+                //From3To4(jsonObj);
+                //From4To5(jsonObj);
+                //From6To7(jsonObj);
+                //From7To8(jsonObj);
+                //From8To9(jsonObj);
+                //From9To10(jsonObj);
+            }
+
+            if (from == 4 && to == 10)
+            {
+                //From4To5(jsonObj);
+                //From6To7(jsonObj);
+                //From7To8(jsonObj);
+                //From8To9(jsonObj);
+                //From9To10(jsonObj);
+            }
+            
+            if (from == 5 && to == 10)
+            {
+                //From6To7(jsonObj);
+                //From7To8(jsonObj);
+                //From8To9(jsonObj);
+                //From9To10(jsonObj);
+            }
+            
+            if (from == 6 && to == 10)
+            {
+                //From7To8(jsonObj);
+                //From8To9(jsonObj);
+                //From9To10(jsonObj);
+            }
+            
+            if (from == 7 && to == 10)
+            {
+                //From8To9(jsonObj);
+                //From9To10(jsonObj);
+            }
+            
+            if (from == 8 && to == 10)
+            {
+                //From9To10(jsonObj);
+            }
+            
+            if (from == 9 && to == 10)
+            {
+                //From9To10(jsonObj);
+            }
+            
+            if (from == 10 && to == 10)
+            {
+                //From9To10(jsonObj);
+            }
+        }
+
+        private static void From1To3(JObject jsonObj)
+        {
+            JToken nameToken = jsonObj["name"];
+            jsonObj.Add("fullName", nameToken);
+
+            JToken ageToken = jsonObj["age"];
+            jsonObj.Add("birthYear", DateTime.Today.Year - ageToken.ToObject<int>());
+            jsonObj.Remove("age");
+
+            var oldNameSplit = nameToken.ToObject<string>()!.Split(' ');
+            jsonObj.Remove("name");
+            jsonObj.Add("name", oldNameSplit[0]);
+            jsonObj.Add("surname", oldNameSplit[1]);
+        }
+    }
+
+    public sealed class PersonJsonCtor : IMigratable
+    {
+        public int Version => 1;
+
+        [NonSerialized] public bool IsMigrationCalled;
+        [NonSerialized] public bool IsJsonCtorCalled;
+
+        [JsonProperty("name")] public string Name;
+        [JsonProperty("age")] public int Age;
+
+        public PersonJsonCtor() { }
+
+        [JsonConstructor]
+        public PersonJsonCtor(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            IsJsonCtorCalled = true;
+        }
+
+        public void Migrate(ref JObject jsonObj, int from, int to)
+        {
+            IsMigrationCalled = true;
+        }
+    }
+
+    public class TwoPersonsNotMigratableMock
+    {
+        [JsonProperty("person1")] public PersonV1 Person1;
+        [JsonProperty("person2")] public PersonV1 Person2;
+    }
+    
+    public class TwoPersonsMigratableMock : IMigratable
+    {
+        public int Version => 1;
+        [NonSerialized] public int MigrationCalledCount;
+
+        [JsonProperty("person1")] public PersonV1 Person1;
+        [JsonProperty("person2")] public PersonV1 Person2;
+
+        public void Migrate(ref JObject jsonObj, int from, int to)
+        {
+            MigrationCalledCount++;
+        }
+    }
+
+    public class PersonsDataStructureMock<T>
+    {
+        [JsonProperty("persons")] public T Persons;
+    }
+}
